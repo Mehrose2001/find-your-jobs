30973e78bf93a40ed9bf173de30bc4dd08d79089
Ash2209 <muhammad.ashar@xpertdigital.co>
2025-06-14 23:10:35 +0500

    deployment fix 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/findYourJobs/",
-  plugins: [react(),tailwindcss(),]
+  plugins: [react(),tailwindcss(),],
+  base: "/findYourJobs/"
 })
